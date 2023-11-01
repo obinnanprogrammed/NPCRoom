db9990a65aa59cb897bbeb871794f13c5334d0b1
obinnanprogrammed <obinnanwakwue10@gmail.com>
2023-11-01 17:49:14 -0500

    basic stuff put in there

--- backend/src/main/java/com/npcroom/NPCRoomApplication.java
+++ backend/src/main/java/com/npcroom/NPCRoomApplication.java
@@ -1,4 +1,11 @@
 package com.npcroom;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NPCRoomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NPCRoomApplication.class, args);
+    }
 }
--- backend/src/main/java/com/npcroom/NPCRoomController.java
+++ backend/src/main/java/com/npcroom/NPCRoomController.java
@@ -1,4 +1,10 @@
 package com.npcroom;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
 public class NPCRoomController {
+    @RequestMapping
+    public String index() { return "Default"; }
 }
