e4120bec94f868d535c870e6f0953e8588f47e2d
obinnanprogrammed <obinnanwakwue10@gmail.com>
2023-11-06 20:49:53 -0600

    forming statements and logic

--- backend/src/main/java/com/npcroom/NPCRoomController.java
+++ backend/src/main/java/com/npcroom/NPCRoomController.java
@@ -1,27 +1,75 @@
 package com.npcroom;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.*;
 
 @RestController
+
 public class NPCRoomController {
+
     @RequestMapping
-    public String index() { return "Default"; }
+    public String processStatement() {
+        Map<String, String> statements = Statements.populateMap();
+        String statement = "I'm good, how are you";
+
+        if(Statements.checkFilter(statement.toLowerCase())) {
+            return "That's a bit inappropriate. Let's talk about something else.";
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        for(String message : statements.keySet()) {
+            if (statement.toLowerCase().contains(message)) {
+                res.append(statements.get(message) + "\n");
+            }
+        }
+        return res.toString();
+    }
+    /*
+    @RequestMapping
+    public String index() {
+        StringBuilder str = new StringBuilder();
+        for (char t = 'a'; t <= 'z'; t++) {
+            str.append(t + " ");
+        }
+        return str.toString();
+    } */
 }
 
 class Statements {
-    private Map<String, String> statementMap;
+    private static Map<String, String> statementMap = new HashMap<String, String>();
+    private static String[] filter = {}; // offensive/inappropriate comments will be censored.
 
-    // consider adding a filter ArrayList
-
-    public Map<String, String> populateMap() {
+    public static Map<String, String> populateMap() {
         statementMap.putIfAbsent("how are you", "I'm good, thanks for asking!");
         statementMap.putIfAbsent("i'm good", "That's good to hear!");
         statementMap.putIfAbsent("not good", "I'm sorry. Care to talk abt it");
-        // above are starters, definitely add more
+        statementMap.putIfAbsent("where are you from", "somewhere");
+        statementMap.putIfAbsent("when were you born", "dude stop");
+        statementMap.putIfAbsent("you suck", "you suck harder");
+        statementMap.putIfAbsent("what is your opinion on artifical intelligence", "REEEEEEE");
+        statementMap.putIfAbsent("what are you doing", "Talking to you ;)");
+        statementMap.putIfAbsent("what are your interests", "What interests, I'm a loser who" +
+                "scrolls Reddit on the daily.");
+        statementMap.putIfAbsent("what do you like to do", "Scroll Reddit");
+        statementMap.putIfAbsent("can we play a game", "Sure!");
+        statementMap.putIfAbsent("i love you", "Wow...I never get that. I love you too!");
+        statementMap.putIfAbsent("i hate you", "Of course you would say that");
+        statementMap.putIfAbsent("what am i supposed to do with this", "I dunno?");
 
         return statementMap;
+    }
+
+    public static boolean checkFilter(String statement) {
+        String[] words = statement.split(" ");
+        for(String word : words) {
+            if(Arrays.asList(filter).contains(word)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 /*
